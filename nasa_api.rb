82768fffb2afea8909e7c71f71b77fc3b421a149
evelyn gomez <evelyngomezfrez@gmail.com>
2020-11-22 19:16:15 -0300

    corrigiendo el codigo para mejorarlo y visualizar la pagina web creada con las fotos de la nasa

--- nasa_api.rb
+++ nasa_api.rb
@@ -2,7 +2,7 @@ require 'json'
 require 'net/http'
 
 def request(address, api_key)
-    url = URI(address+"&api_key="+api_key)
+    url = URI(address+api_key)
     #debo concatenar la api key en la url
     
     http = Net::HTTP.new(url.host, url.port)
@@ -15,32 +15,40 @@ def request(address, api_key)
     #es necesario requerirlo y en IRB se escribe require 'json'
 end
 
-    body = "https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=DEMO_KEY&page=1&"
-    body = body["photos"]
-
-#crear un metodo llamado buid_web_page
-def buid_web_page(hash)#que reciba en hash de respuesta
+#crear un metodo llamado build_web_page
+def build_web_page(hash)#que reciba en hash de respuesta
     #con todos los datos y construya una pagina web
     #se evalúa la pagina creada y tiene que tener formato
+    photos = hash["photos"]
 
-    html = "<html>\n<title>Api Nasa</title>\n<head>\n</head>\n<body>\n<h2>Api Nasa</h2>\n<ul>\n"
-    hash.map {|img| html += "\t<li><img src=\"#{img["img_src"]}\"></li>\n"}
+    html = "<html>\n<title>Fotos Api Nasa</title>\n<head>\n</head>\n<body>\n<h2>Fotos Api Nasa</h2>\n<ul>\n"
+    photos.map {|img| html += "\t<li><img src=#{img["img_src"]}></li>\n"}
     html += "</ul>\n</body>\n</html>"
     File.write('nasa_api.html', html)
 end
 
-buid_web_page(body)
+def photos_count(data)
+    photos_array = data["photos"]#accediendo al arrelgo fotos
+    final_array = []
+   
+    photos_array.each do |i|
+        final_array << i['camera']['name']
 
-
-#pregunta bonus, crear metodo 
-def photos_count(hash)#que reciba un hash de respuesta
-    #devuelva new_hash #con el nombre de la camara y cantidad de fotos
-
-    hash = request("https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=DEMO_KEY&page=1")[0..25]#limitamos los restultados a 25
-    photos = hash.map {|x| x['url']}
-    html = ""
-    photos.each do |photo|
-    html += "<img src=\"#{photo}\">\n"
     end
+
+    final_hash = final_array.group_by {|x| x}#esta agrupando todos los nombres
+    final_hash.each do |k,v|#
+        final_hash[k] = v.count
+    end
+
+    final_hash
 end
+
+    data = request("https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000", "&api_key=0IKxbe4GcwTwCfJsbKzegTyslzF6NhVE1gHkgRgy&page=1")
+    build_web_page(data)
+    print photos_count(data)
+
+
+
+
 
